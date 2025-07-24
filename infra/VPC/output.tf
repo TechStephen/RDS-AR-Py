2c907b469d04193e6bcb5c154fb5df57f85e5130
TechStephen <stephenschmitz518@gmail.com>
2025-07-24 12:16:01 -0400

    changes

--- infra/VPC/output.tf
+++ infra/VPC/output.tf
@@ -5,7 +5,3 @@ output "vpc_id" {
 output "subnet_group_name" {
   value = aws_db_subnet_group.subnet_group.name
 }       
-
-output "app_runner_service_url" {
-  value = module.AppRunner.service_url
-}
